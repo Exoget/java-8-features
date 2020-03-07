2d5ab2f97a470b7e8c0d83da7a571507919f0265
Exoget <ferchichi.feres@gmail.com>
2020-03-07 21:26:01 +0100

    first commit

---
+++ src/main/java/com/sofrateam/javafeatures/LignesDeCommandes.java
@@ -0,0 +1,9 @@
+package com.sofrateam.javafeatures;
+
+public class LignesDeCommandes implements Cloneable{
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
---
+++ src/main/java/com/sofrateam/javafeatures/Personne.java
@@ -0,0 +1,91 @@
+package com.sofrateam.javafeatures;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Personne {
+
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private Boolean valide;
+    private int numero;
+    private static int nbInstance;
+
+    // il faut tjr creer un constructuer par defaut pour garantir le chinage des constructuer vers la class Objet
+    public Personne() {
+        nbInstance++;
+        this.numero = nbInstance;
+    }
+
+    public Personne(String nom, String prenom, LocalDate dateNaissance) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        nbInstance++;
+        this.numero = nbInstance;
+    }
+
+    public Boolean isValide() {
+        return valide;
+    }
+
+    // si le type booelan est utilise en lecture seul on peut eliminer cette fonction
+    public void setValide(Boolean valide) {
+        this.valide = valide;
+    }
+
+    public long calculAge() {
+        return dateNaissance.until(LocalDate.now(), ChronoUnit.YEARS);
+    }
+
+    //@Deprecated
+    public void afficher() {
+        System.out.println("nom : " + nom);
+        System.out.println("prenom : " + prenom);
+        System.out.println("age : " + calculAge());
+    }
+
+    /*
+    Fonction concu pour accepter un nombre variable de parametres
+    exemple d'appel
+    afficher("rouge")
+    afficher("orange", "blue")
+    afficher(null) pour forcer le passage par cette methode et non pas afficher()
+     */
+    public void afficher(String... params) {
+        System.out.println("nombre de parametres : " + params.length);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom.toUpperCase();
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom.toLowerCase();
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public static int getNbInstance() {
+        return nbInstance;
+    }
+
+    public static void setNbInstance(int nbInstance) {
+        Personne.nbInstance = nbInstance;
+    }
+}
