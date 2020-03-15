d8c6b6d4e551e1705fc469502e4fe4c4a068fe95
Exoget <ferchichi.feres@gmail.com>
2020-03-15 23:27:10 +0100

    adding comment to Consumer class

--- src/main/java/com/sofrateam/interfaces/consumer/TestConsumer.java
+++ src/main/java/com/sofrateam/interfaces/consumer/TestConsumer.java
@@ -1,8 +1,16 @@
 package com.sofrateam.interfaces.consumer;
 
-
 import java.util.function.Consumer;
-
+/*
+ Class contenant des examples sur l'utilisation des interface fonctionnelle de type Consumer
+ #Consumer<T>    : une interface fonctionnelle avec une methode accept() qui prend le type <T> et renvoi rien
+ #DoubleConsumer : une interface fonctionnelle avec une methode accept() qui prend le type Double et renvoi rien
+ #IntConsumer  : une interface fonctionnelle avec une methode accept() qui prend types Int et renvoi rien
+ #LongConsumer : une interface fonctionnelle avec une methode accept() qui prend Long et renvoi rien
+ #ObjDoubleConsumer; ObjIntConsumer; ObjLongConsumer : des consumer avec un objet et un parametre
+ autres Methodes implementer par defaut:
+    andThen(Consumer<T>)  :applique le Consumer courante puis celui en parametre
+ */
 class Fichier {
 
     private String tile;
@@ -28,7 +36,6 @@ public class TestConsumer {
 
     public static void main(String args[]) {
         Fichier file = new Fichier("JAVA_8_features");
-
         file.affiche(f -> System.out.println(f.getTile()));
     }
 }
