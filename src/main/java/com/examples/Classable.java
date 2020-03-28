28e5758b83c6f414ba919868ab1144733733d9fa
fferchichi <ferchichi.feres@gmail.com>
2020-03-28 07:09:26 +0100

    all filed are implicitly public and static inside the interface

--- src/main/java/com/examples/Classable.java
+++ src/main/java/com/examples/Classable.java
@@ -9,11 +9,24 @@ public interface Classable {
     @return 0 si les deux sont égaux
     @retunr -99 si comparaison imposssible
      */
+    // les modifier sont par defaut public et abstract
     int compare(Object o);
 
-    public static final int SUPERIEUR = 1;
-    public static final int INFERIEUR = -1;
-    public static final int EGAL = 0;
-    public static final int ERREUR = -99;
+    default boolean isInferieur(Object o) {
+        return false;
+    }
+
+    default boolean isSuperieur(Object o) {
+        return false;
+    }
+
+    /*
+    Every field declaration in the body of an interface is implicitly public, static, and final.
+    It is permitted to redundantly specify any or all of these modifiers for such fields.
+     */
+    int SUPERIEUR = 1;
+    int INFERIEUR = -1;
+    int EGAL = 0;
+    int ERREUR = -99;
 
 }
