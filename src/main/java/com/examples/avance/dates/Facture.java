fcb062ad421e9d1c30df15115fe25a6763941001
Exoget <ferchichi.feres@gmail.com>
2020-04-11 11:21:03 +0200

    delete unused import

--- src/main/java/com/examples/avance/dates/Facture.java
+++ src/main/java/com/examples/avance/dates/Facture.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
-import java.time.format.TextStyle;
 import java.time.temporal.ChronoField;
 
 import static java.time.temporal.ChronoUnit.DAYS;
